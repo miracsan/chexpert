fixed numerical isntability in MultitaskLearningAnchor

lossFunctions.py
@@ -84,10 +84,10 @@ class MultitaskLearningAnchorLoss(torch.nn.Module) :
         
         thresh = target * pos_probs + inv_target * neg_probs - self.sigma
         
-        pos_cost_terms_1 = - (target * torch.log(pos_probs))
-        pos_cost_terms_2 = - (inv_target * torch.pow((1 + pos_probs - thresh), self.gamma) * torch.log(1 - pos_probs))
-        neg_cost_terms_1 = - (inv_target * torch.log(neg_probs))
-        neg_cost_terms_2 = - (target * torch.pow((1 + neg_probs - thresh), self.gamma) * torch.log(1 - neg_probs))
+        pos_cost_terms_1 = - (target * torch.log(pos_probs + 1e-12))
+        pos_cost_terms_2 = - (inv_target * torch.pow((1 + pos_probs - thresh), self.gamma) * torch.log(1 - pos_probs + 1e-12))
+        neg_cost_terms_1 = - (inv_target * torch.log(neg_probs + 1e-12))
+        neg_cost_terms_2 = - (target * torch.pow((1 + neg_probs - thresh), self.gamma) * torch.log(1 - neg_probs + 1e-12))
         
         total_cost_terms = pos_cost_terms_1 + pos_cost_terms_2 + neg_cost_terms_1 + neg_cost_terms_2        
         weighted_cost_terms = total_cost_terms * inter_class_weights        

trainCnn.py
@@ -136,7 +136,7 @@ def train_cnn(PATH_TO_MAIN_FOLDER, LR, WEIGHT_DECAY, USE_MODEL=0,UNCERTAINTY="ze
         elif UNCERTAINTY == 'effective_num_zeros':
             intra_class_weights=transformed_datasets['train'].effective_num_weights()
             print("intra_class_weights: {0}".format(intra_class_weights))
-            criterion = nn.BCEWithLogitsLoss(pos_weight=intra_class_weights)
+            criterion = nn.BCEWithLogitsLoss(pos_weight=torch.cuda.FloatTensor(intra_class_weights))
         elif UNCERTAINTY == 'effective_num_gradnorm_zeros':
             intra_class_weights=transformed_datasets['train'].effective_num_weights()
             print("intra_class_weights: {0}".format(intra_class_weights))